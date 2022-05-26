ecca11de8fb2f608db066c16d66c4c1044886bee
Scott McPeak <scott.g.mcpeak@gmail.com>
2022-05-26 09:05:11 -0700

    gnu.gr: Tolerate (and ignore) "static" inside array declarator brackets.

--- test/tcheck/c99-array-declarator-qualifier.c
+++ test/tcheck/c99-array-declarator-qualifier.c
@@ -15,4 +15,22 @@ int f2o(int [3]);
 int f3o(int [const]);
 int f4o(int [const 4]);
 
+// 'static', then optional CV, then required expression.
+int g1(int a[static 3]);
+int g2(int a[static volatile 3]);
+int g1o(int [static 3]);
+int g2o(int [static volatile 3]);
+
+// CV, then 'static', then required expression.
+int h1(int a[const static 3]);
+int h2(int a[const volatile static 3]);
+int h1o(int [const static 3]);
+int h2o(int [const volatile static 3]);
+
+// Interestingly, it is not legal to intermix CV and 'static'.
+//int nope1(int a[const static volatile 3]);
+
+// I'm missing the case of '*' as the size.  For the moment I don't need
+// it.
+
 // EOF
