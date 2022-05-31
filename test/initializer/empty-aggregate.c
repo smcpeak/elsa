f5031881a55e18dfcdfe18c664733ee60a3c7842
Scott McPeak <scott.g.mcpeak@gmail.com>
2022-05-31 16:53:35 -0700

    Add test/initializer/empty-aggregate.c.
    test/initializer/Makefile: Add {gcc,clang,all}-relaxed-check.

---
+++ test/initializer/empty-aggregate.c
@@ -0,0 +1,119 @@
+// empty-aggregate.c
+// Test with empty aggregates: zero-length array or empty struct.
+
+// Zero-length arrays are prohibited by C11 6.7.6.2p1: "If the
+// expression is a constant expression, it shall have a value greater
+// than zero."  (And p5 precludes zero-length VLAs.)
+
+// Empty structs are prohibited by the grammar in C11 6.7.2.1p1, which
+// has "struct-declaration-list" inside the braces, without any "opt"
+// notation.  (But C++ allows them.)
+
+// However, these are allowed as GNU extensions:
+// * https://gcc.gnu.org/onlinedocs/gcc/Zero-Length.html
+// * https://gcc.gnu.org/onlinedocs/gcc/Empty-Structures.html
+
+
+int arr0[0] = {};
+
+static int test_arr0()
+{
+  _Static_assert(sizeof(arr0) == 0, "");
+  return 1;
+}
+
+
+int arr20[2][0] = {};
+
+static int test_arr20()
+{
+  _Static_assert(sizeof(arr20) == 0, "");
+  return 1;
+}
+
+
+int arr02[0][2] = {};
+
+static int test_arr02()
+{
+  _Static_assert(sizeof(arr02) == 0, "");
+  return 1;
+}
+
+
+int arr00[0][0] = {};
+
+static int test_arr00()
+{
+  _Static_assert(sizeof(arr00) == 0, "");
+  return 1;
+}
+
+
+typedef struct Empty {
+} Empty;
+
+Empty empty = {};
+
+static int test_empty()
+{
+  _Static_assert(sizeof(empty) == 0, "");
+  return 1;
+}
+
+
+typedef struct IEI {
+  int x;
+  Empty empty;
+  int y;
+} IEI;
+
+// Somewhat curiously, GCC requires explicit braces when initializing an
+// empty structure, rather than just skipping over them.  Its error
+// message is a bit vague (actually a warning, because it just swallows
+// the next initializer).  Clang gives a clear error message.  My guess
+// is the GCC limitation was unintentional and Clang is enforcing GCC's
+// accidental rule.
+IEI iei1 = { 1, {}, 2 };
+
+static int test_iei()
+{
+  return
+    iei1.x == 1 &&
+    iei1.y == 2 &&
+    1;
+}
+
+
+typedef struct I_arr0_I {
+  int x;
+  int arr0[0];
+  int y;
+} I_arr0_I;
+
+I_arr0_I iai1 = { 1, {}, 2 };
+
+static int test_iai()
+{
+  return
+    iai1.x == 1 &&
+    iai1.y == 2 &&
+    1;
+}
+
+
+int main()
+{
+  return
+    test_arr0() &&
+    test_arr20() &&
+    test_arr02() &&
+    test_arr00() &&
+    test_empty() &&
+    test_iei() &&
+    test_iai() &&
+    1? 0 : 1;
+}
+
+
+// EOF
