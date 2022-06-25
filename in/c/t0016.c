e36e61fcd2bc9473fcc0bf3d6bd717d879e91651
Scott McPeak <scott.g.mcpeak@gmail.com>
2022-06-25 14:34:08 -0700

    test/Makefile: Add t0015.c and t0016.c was working with --clang.

--- in/c/t0016.c
+++ in/c/t0016.c
@@ -3,8 +3,12 @@
 
 int fallowc(int);
 
+// When this is pretty-printed in --clang mode, it prints as
+// "int fallowc(char c) { ... }", which is then not accepted as input.
+// For now I'm just going to accept that and hope this does not arise
+// in practice.
 fallowc(c)
 char c;        // "default argument promotions" make this int
 {
-  return 0;
+  return c;
 }
